eb3947531ce4463887f62960309342e72630bea7
Ryan Henderson <ryanehenderson@protonmail.com>
2023-01-19 17:53:12 -0600

    Implement embed creation and updating

--- deploy/deploy-commands-guild.js
+++ deploy/deploy-commands-guild.js
@@ -10,43 +10,8 @@ const commands = [
                 .setDescription('Creates a new embed')
                 .addStringOption(option =>
                     option
-                        .setName('title')
-                        .setDescription('Title of the embed')
-                        .setRequired(false)
-                )
-                .addStringOption(option =>
-                    option
                         .setName('content')
                         .setDescription('Content of the embed')
-                        .setRequired(false)    
-                )
-                .addStringOption(option =>
-                    option
-                        .setName('footer')
-                        .setDescription('Footer of the embed')
-                        .setRequired(false)    
-                )
-                .addStringOption(option =>
-                    option
-                        .setName('color')
-                        .setDescription('Hex color of the embed (no #)')
-                        .setRequired(false)    
-                )
-                .addChannelOption(option =>
-                    option
-                        .setName('channel')
-                        .setDescription('Channel to post the embed in')
-                        .setRequired(false)
-                )
-        )
-        .addSubcommand(subcommand =>
-            subcommand
-                .setName('update')
-                .setDescription('Updates an existing embed created by this bot')
-                .addStringOption(option =>
-                    option
-                        .setName('message')
-                        .setDescription('Message ID of the message to update')
                         .setRequired(true)    
                 )
                 .addStringOption(option =>
@@ -57,18 +22,6 @@ const commands = [
                 )
                 .addStringOption(option =>
                     option
-                        .setName('content')
-                        .setDescription('Content of the embed')
-                        .setRequired(false)    
-                )
-                .addStringOption(option =>
-                    option
-                        .setName('footer')
-                        .setDescription('Footer of the embed')
-                        .setRequired(false)    
-                )
-                .addStringOption(option =>
-                    option
                         .setName('color')
                         .setDescription('Hex color of the embed (no #)')
                         .setRequired(false)    
@@ -76,8 +29,43 @@ const commands = [
                 .addChannelOption(option =>
                     option
                         .setName('channel')
-                        .setDescription('Channel to post the embed in')
+                        .setDescription('Channel to post the embed in (defaults to current channel)')
                         .setRequired(false)
+                )
+        )
+        .addSubcommand(subcommand =>
+            subcommand
+                .setName('update')
+                .setDescription('Updates an existing embed created by this bot')
+                .addStringOption(option =>
+                    option
+                        .setName('content')
+                        .setDescription('Content of the embed')
+                        .setRequired(true)    
+                )
+                .addStringOption(option =>
+                    option
+                        .setName('message')
+                        .setDescription('Message ID of the message to update')
+                        .setRequired(true)    
+                )
+                .addChannelOption(option =>
+                    option
+                        .setName('channel')
+                        .setDescription('Channel the message is in (defaults to current channel)')
+                        .setRequired(false)
+                )
+                .addStringOption(option =>
+                    option
+                        .setName('title')
+                        .setDescription('Title of the embed')
+                        .setRequired(false)
+                )
+                .addStringOption(option =>
+                    option
+                        .setName('color')
+                        .setDescription('Hex color of the embed (no #)')
+                        .setRequired(false)    
                 )
         )
 ]
--- index.js
+++ index.js
@@ -15,13 +15,54 @@ client.once('ready', () => {
     console.log(`Logged in as ${client.user.tag}`);
 });
 
+// Command handler
+client.on('interactionCreate', async (interaction) => {
+    if (!interaction.isChatInputCommand()) {
+        return;
+    }
+
+    const { commandName } = interaction;
+
+    if (commandName === 'embed') {
+        const subcommand = interaction.options.getSubcommand();
+
+        const title = interaction.options.getString('title');
+        const content = interaction.options.getString('content');
+        const footer = interaction.options.getString('footer');
+        const color = interaction.options.getString('color');
+
+        const embed = makeEmbed(title, content, footer, color);
+
+        let channel = interaction.options.getChannel('channel');
+        if (channel === null) {
+            channel = interaction.channel;
+        }
+
+        if (subcommand === 'create') {
+            channel.send({ embeds: [embed] });
+            interaction.reply({ content: 'Done!', ephemeral: true });
+        } else if (subcommand === 'update') {
+            const messageId = interaction.options.getString('message');
+            channel.messages.fetch(messageId).then((message) => {
+                if (message.author !== client.user) {
+                    interaction.reply({ content: 'Error: Cannot edit a message sent by a different user', ephemeral: true });
+                    return;
+                }
+                message.edit({ embeds: [embed] });
+                interaction.reply({ content: 'Done!', ephemeral: true });
+            }).catch(() => {
+                interaction.reply({ content: `Error: Could not find message with ID ${messageId} in channel ${channel}`, ephemeral: true })
+            })
+        }
+    }
+});
+
 function makeEmbed(title, content, footer, color) {
     return new EmbedBuilder()
         .setTitle(title)
         .setDescription(content)
         .setFooter(footer)
-        .setColor(color)
-        .build();
+        .setColor(color);
 }
 
 client.login(discordToken);
