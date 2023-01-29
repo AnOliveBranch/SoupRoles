ccbddedddf74fc96ab424bf3512553298d582616
Ryan Henderson <ryanehenderson@protonmail.com>
2023-01-29 13:44:15 -0600

    Implement creating menu selector from file

--- index.js
+++ index.js
@@ -4,8 +4,11 @@ const {
     EmbedBuilder,
     ActionRowBuilder, 
     ButtonBuilder, 
-    ButtonStyle
+    ButtonStyle,
+    StringSelectMenuBuilder
 } = require('discord.js');
+const fs = require('fs');
+const yaml = require('js-yaml');
 const {
     discordToken
 } = require('./config.json');
@@ -14,9 +17,32 @@ const client = new Client({
     intents: [GatewayIntentBits.Guilds]
 });
 
+let serverData = new Map();
+
 client.once('ready', () => {
+    fs.promises.mkdir('./data', { recursive: true }).then(() => {
+        client.guilds.cache.forEach((guild) => {
+            loadData(guild.id);
+        });
+    });
+    
     console.log(`Logged in as ${client.user.tag}`);
 });
+
+async function loadData(guildId) {
+    return fs.promises.readFile(`./data/${guildId}.yml`, 'utf8').then((data) => {
+        let yamlData = yaml.load(data);
+        serverData.set(guildId, yamlData);
+    }).catch(() => {
+        serverData.set(guildId, {});
+        saveData(guildId);
+    });
+}
+
+async function saveData(guildId) {
+    let guildData = yaml.dump(serverData.get(guildId));
+    return fs.promises.writeFile(`./data/${guildId}.yml`, guildData, 'utf8');
+}
 
 // Command handler
 client.on('interactionCreate', async (interaction) => {
@@ -31,10 +57,10 @@ client.on('interactionCreate', async (interaction) => {
 
         const title = interaction.options.getString('title');
         const content = interaction.options.getString('content');
-        const footer = interaction.options.getString('footer');
+
         const color = interaction.options.getString('color');
 
-        const embed = makeEmbed(title, content, footer, color);
+        const embed = makeEmbed(title, content, color);
 
         const channel = interaction.options.getChannel('channel') === null ? interaction.channel : interaction.options.getChannel('channel');
  
@@ -81,7 +107,7 @@ client.on('interactionCreate', async (interaction) => {
 
                 let button = makeButton(buttonId, title);
                 buttons.push(button);
-                let rows = buildComponents(buttons);
+                let rows = buildButtonComponents(buttons);
                 message.edit({ components: rows });
                 interaction.reply({ content: 'Done!', ephemeral: true });
             } else if (subcommand === 'delete') {
@@ -95,7 +121,7 @@ client.on('interactionCreate', async (interaction) => {
                 }
 
                 buttons.splice(buttonIndex, 1);
-                let rows = buildComponents(buttons);
+                let rows = buildButtonComponents(buttons);
                 message.edit({ components: rows });
                 interaction.reply({ content: 'Done!', ephemeral: true });
             } else if (subcommand === 'update') {
@@ -111,7 +137,7 @@ client.on('interactionCreate', async (interaction) => {
                 let button = makeButton(buttonId, title);
                 buttons[buttonIndex] = button;
 
-                let rows = buildComponents(buttons);
+                let rows = buildButtonComponents(buttons);
                 message.edit({ components: rows });
                 interaction.reply({ content: 'Done!', ephemeral: true });
             } else if (subcommand === 'get') {
@@ -129,12 +155,68 @@ client.on('interactionCreate', async (interaction) => {
     }
 });
 
+// Button handler
+client.on('interactionCreate', async (interaction) => {
+    if (!interaction.isButton()) {
+        return;
+    }
+
+    const messageId = interaction.message.id;
+    const buttonId = interaction.customId;
+    const serverRules = serverData.get(interaction.guild.id);
+
+    // Make sure there's a messages list to handle
+    if (!serverRules.hasOwnProperty('messages')) {
+        return;
+    }
+
+    const messageRules = serverRules['messages'][messageId];
+
+    // Make sure the message has defined rules
+    if (messageRules === undefined) {
+        return;
+    }
+
+    const buttonRules = messageRules[buttonId];
+
+    // Make sure the button has defined rules
+    if (buttonRules === undefined) {
+        return;
+    }
+
+    let embedRules = buttonRules['embed'];
+
+    // Title and color are not required, null them if not present (EmbedBuilder accepts null)
+    if (embedRules['title'] === undefined) {
+        embedRules['title'] = null;
+    }
+    if (embedRules['color'] === undefined) {
+        embedRules['color'] = null;
+    }
+
+    const embed = makeEmbed(embedRules['title'], embedRules['text'], embedRules['color']);
+    
+    let roles = [];
+
+    await buttonRules['roles'].forEach((roleId) => {
+        interaction.guild.roles.fetch(roleId).then((role) => {
+            roles.push(role);
+        }).catch((err) => {
+            console.log(`Role with ID ${roleId} couldn't be found`);
+            console.log(err);
+        });
+    });
+
+    const component = buildMenuComponents(roles);
+
+    interaction.reply({ embeds: [embed], components: [component], ephemeral: true });
+});
+
 // Returns a new embed
-function makeEmbed(title, content, footer, color) {
+function makeEmbed(title, content, color) {
     return new EmbedBuilder()
         .setTitle(title)
         .setDescription(content)
-        .setFooter(footer)
         .setColor(color);
 }
 
@@ -149,8 +231,8 @@ function makeButton(id, title) {
 // Returns an array of buttons in a message
 function getButtons(message) {
     let buttons = [];
-    message.components.forEach(function (componentRow) {
-        componentRow.components.forEach(function (component) {
+    message.components.forEach((componentRow) => {
+        componentRow.components.forEach((component) => {
             buttons.push(component);
         });
     });
@@ -158,7 +240,7 @@ function getButtons(message) {
 }
 
 // Returns an array of ActionRowBuilders, populated with the contents of the "buttons" array
-function buildComponents(buttons) {
+function buildButtonComponents(buttons) {
     let rows = [];
     for (let i = 0; i < buttons.length; i++) {
         let row;
@@ -182,6 +264,27 @@ function getButtonIndex(buttons, buttonId) {
         }
     }
     return index;
+}
+
+function buildMenuComponents(roles) {
+    let row = new ActionRowBuilder();
+    let menuBuilder = new StringSelectMenuBuilder()
+        .setCustomId('selector')
+        .setPlaceholder('Select role(s)')
+        .setMaxValues(roles.length);
+
+    roles.forEach((role) => {
+        console.log(role);
+        menuBuilder.addOptions(
+            {
+                label: role.name,
+                value: role.id
+            }
+        );
+    });
+
+    row.addComponents(menuBuilder);
+    return row;
 }
 
 client.login(discordToken);
