b5a0a937ad0da2c0797ae6ddf5c109cbeb032612
AnOliveBranch <liv@anolivebranch.dev>
2024-08-20 08:33:41 -0500

    Finish translator

--- translateData.js
+++ translateData.js
@@ -4,60 +4,40 @@ const { logLevel, channels } = require('./config.json');
 logger.level = logLevel;
 
 const yaml = require('js-yaml');
-const fsPromises = require('node:fs/promises');
+const fs = require('fs');
 const path = require('node:path');
 const { RoleManager } = require('./util/RoleManager');
 
-const roleManager = new RoleManager();
-roleManager
-	.init()
-	.then(() => {
-		const dataFolder = path.join(__dirname, '.', 'data');
-		fsPromises
-			.readdir(dataFolder)
-			.then((dataFiles) => {
-				dataFiles.forEach((fileName) => {
-					if (fileName.endsWith('.yml')) {
-						fsPromises
-							.readFile(path.join(__dirname, '.', 'data', fileName))
-							.then((content) => {
-								const messageData = yaml.load(content)['messages'];
-								if (messageData === undefined) {
-									return;
-								}
-								Object.entries(messageData).forEach((message) => {
-									const [messageId, buttons] = message;
-									Object.entries(buttons).forEach((button) => {
-										const [buttonId, roles] = button;
-										roleManager
-											.setRoles(messageId, buttonId, roles['roles'])
-											.then(() => {
-												logger.info(
-													`Successfully set roles for button ${buttonId} on message ${messageId}`
-												);
-											})
-											.catch((error) => {
-												logger.warn(
-													`Failed to set roles for button ${buttonId} on message ${messageId}`
-												);
-												logger.error(error);
-											});
-									});
-								});
-							})
-							.catch((error) => {
-								logger.error(`Failed to read data file ${fileName}`);
-								logger.error(error);
-							});
-					}
-				});
-			})
-			.catch((error) => {
-				logger.error('Failed to read data folder');
-				logger.fatal(error);
+const dataFolder = path.join(__dirname, '.', 'data');
+const dataFile = path.join(__dirname, '.', 'data', 'roleData.json');
+let roleDataContent = {};
+const files = fs.readdirSync(dataFolder);
+files.forEach((fileName) => {
+	if (fileName.endsWith('.yml')) {
+		const content = fs.readFileSync(path.join(__dirname, '.', 'data', fileName));
+		const messageData = yaml.load(content)['messages'];
+		if (messageData === undefined) {
+			return;
+		}
+		Object.entries(messageData).forEach((message) => {
+			const [messageId, buttons] = message;
+			Object.entries(buttons).forEach((button) => {
+				const [buttonId, roles] = button;
+				const roleData = roles['roles'];
+				logger.debug(buttonId);
+				let buttonData = roleData;
+				logger.debug(buttonData);
+				let newMessageData = {};
+				newMessageData[buttonId] = buttonData;
+				logger.debug(newMessageData);
+				if (!roleDataContent[messageId]) {
+					roleDataContent[messageId] = {};
+				}
+				roleDataContent[messageId][buttonId] = roleData;
+				// logger.debug(roleDataContent);
 			});
-	})
-	.catch((error) => {
-		logger.error('Failed to initialize Role Manager');
-		logger.fatal(error);
-	});
+		});
+	}
+});
+
+fs.writeFileSync(dataFile, JSON.stringify(roleDataContent));
